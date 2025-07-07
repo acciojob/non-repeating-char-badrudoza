0c82ce47399baadf5537987e1a4a8246427cf6f0
badrudoza <106438583+badrudoza@users.noreply.github.com>
2025-07-07 09:39:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let obj={};
+	for(let i=0;i<str.length;i++){
+		obj[str[i]]=(obj[str[i]] || 0)+1;
+	}
+	for(let [key,value] of Object.entries(obj)){
+		if(value===1){
+			return key;
+		}	
+	}
+	return null;	
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
